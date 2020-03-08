98582d7a8d9c179453becf6d535c761cc92fe3bf
rupomsoft <33941621+rupomsoft@users.noreply.github.com>
2020-03-09 03:01:47 +0600

    15 Basic Routing Group
    15 Basic Routing Group

--- Laravel Project/blog/resources/views/DemoView.blade.php
+++
@@ -1,3 +0,0 @@
-<h1>First Name: {{$firstKey}} </h1>
-<h1>Middle Name: {{$middleKey}}</h1>
-<h1>First Name:  {{$lastKey}}</h1>
--- Laravel Project/blog/routes/web.php
+++ Laravel Project/blog/routes/web.php
@@ -2,4 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/Name/{firstName}/{MiddleName}/{lastName}','DemoController@MyName');
+
+Route::group(['prefix'=>'account'],function(){
+
+    Route::get('/profile', function(){
+            return "Profile";
+    });
+
+
+    Route::get('/login',function(){
+        return "login";
+    } );
+
+
+    Route::get('/logout', function(){
+
+        return "logout";
+
+    } );
+    Route::get('/signup', function(){
+
+        return "signup";
+    } );
+
+
+    Route::get('/updateprofile', function(){
+
+        return "updateprofile";
+    } );
+
+
+
+});
+
+
+
